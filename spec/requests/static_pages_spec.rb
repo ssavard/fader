647c99f47244e9def24a44054dd003f1b5bd75e7
ssavard <s@rfp.bz>
2014-01-10 14:53:48 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Fader'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Fader')
-    end
+    it { should have_content('Fader') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
-  
+
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
+
+
 end
