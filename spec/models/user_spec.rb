ddda0c18267c0eada00ceb12f609ba52b351ca73
ssavard <s@rfp.bz>
2014-01-11 20:13:37 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,14 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   
   describe "when email format is invalid" do
     it "should be invalid" do
