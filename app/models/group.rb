34334797a6af65d85de0345dc154c6948916c7b4
ssavard <s@rfp.bz>
2014-01-23 14:58:26 -0500

    added geocoder

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :longitude, :allow_nil => true, numericality: true
+  validates :latitude, :allow_nil => true, numericality: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,6 @@
 class Market < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
-  validates :rank, presence: true, numericality: { only_integer: true }
+  validates :rank, presence: true, numericality: { only_integer: true }, uniqueness: true
+  validates :longitude, :allow_nil => true, numericality: true
+  validates :latitude, :allow_nil => true, numericality: true
 end
