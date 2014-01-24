86dddacab92a48742e03379d6d920c4205082a5d
ssavard <s@rfp.bz>
2014-01-24 09:21:37 -0500

    added styling and links

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :stations, inverse_of: :group
-  has_many :stations, inverse_of: :affiliate
+  has_many :affiliates, class_name: "Station", foreign_key: "affiliate_id", inverse_of: :affiliate
+  
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :longitude, :allow_nil => true, numericality: true
   validates :latitude, :allow_nil => true, numericality: true
